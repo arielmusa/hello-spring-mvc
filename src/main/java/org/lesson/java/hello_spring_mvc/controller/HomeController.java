0659694a0b468d4b77b7830858215d80dfd2a693
arielmusa <arielmusainfo@gmail.com>
2025-10-04 21:49:53 +0200

    add /about get route and template

--- src/main/java/org/lesson/java/hello_spring_mvc/controller/HomeController.java
+++ src/main/java/org/lesson/java/hello_spring_mvc/controller/HomeController.java
@@ -1,8 +1,11 @@
 package org.lesson.java.hello_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +14,13 @@ public class HomeController {
     @GetMapping("/")
     public String Home(){
         return "index";
-    }    
+    }
+
+    @GetMapping("/about")
+    public String getMethodName(Model model, @RequestParam(name = "name") String name) {
+        model.addAttribute("name", name);
+
+        return "about";
+    }
+    
 }
